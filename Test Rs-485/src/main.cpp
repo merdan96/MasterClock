fa09d656397dd3a447a7d6d629afc36b3c547011
Ebrahim Hamouda <ebrahim.hamouda25@gmail.com>
2021-01-14 13:18:53 +0200

    Validation from master 2 slaves

--- Test Rs-485/src/main.cpp
+++ Test Rs-485/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "Rs_485.h"
 
+uint32_t Sys_Tick = 0, Count = 0;
+
 void setup()
 {
   // set the data rate for the SoftwareSerial port
@@ -15,19 +17,40 @@ void setup()
  * Serve Request
  */
 // From Master 2 Slave
-//char Data[] = "#13:55";
-//char Data[] = "@0001U0120";
+char Data[][10] =
+    {
+        "#13:55",
+        "@0001U0120",
+        "&0001A",
+        "&0001E",
+        "&0001S",
+        "&0001P"};
 
-// // From Slave 2 Masters
-char Data[] = "&0001A"; // Ack for being online.
-// char Data[] = "&0001E"; // Exam meaning not yet started.
-// char Data[] = "&0001S"; // Start meaning exam started.
-// char Data[] = "&0001P"; // Paused meaing it stopped now.
+void Network_TxBroadCasting()
+{
+  if (Count < 6)
+  {
+    Rs485_Tx(Data[Count++]);
+  }
+  else
+  {
+    Count = 0;
+  }
+}
+
+void Network_RxMainFunction()
+{
+  Rs485_RxMainFunction();
+}
 
 void loop()
 {
   // run over and over
-  Rs485_Tx(Data);
-  // Rs485_RxMainFunction();
-  delay(3000);
+  if (Sys_Tick % 30 == 0)
+  {
+    Network_TxBroadCasting();
+  }
+  Network_RxMainFunction();
+  delay(100);
+  Sys_Tick++;
 }
